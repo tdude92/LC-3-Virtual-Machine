a9c388e8c9d8c3fda7ccd1957ad7947213521518
tdude92 <trevorguanhao@gmail.com>
2020-09-16 23:16:21 -0400

    implement some operations

--- src/lc3.cpp
+++ src/lc3.cpp
@@ -1,3 +1,11 @@
+#define LC3_DEBUG_BUILD
+
+#ifdef LC3_DEBUG_BUILD
+    #define DEBUG(x) do{x}while(0)
+#else
+    #define DEBUG(x) do{}while(0)
+#endif
+
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
@@ -15,9 +23,13 @@ uint16_t lc3_memread(uint16_t addr) {
     return mem[addr];
 }
 
-uint16_t padSignedBits(uint16_t x, int nbits) {
-    // Pad a n bit number to its 16 bit two's complement representation.
-    if ((x >> (nbits - 1)) & 1)
+void lc3_memwrite(uint16_t addr, uint16_t x) {
+    mem[addr] = x;
+}
+
+uint16_t signExtend(uint16_t x, int nbits) {
+    // Pad a n-bit number to its 16-bit two's complement representation.
+    if ((x >> (nbits - 1)) & 0x1)
         // Pad negative number with 1
         return x | 0xFFFF << nbits;
     // Pad positive number with 0
@@ -93,47 +105,78 @@ int main(int argc, char *argv[]) {
 
     uint16_t instr, op;
     bool running = true;
-    int ctr = 0;
     while (running) {
         instr = lc3_memread(reg[R_PC]++); // Read instruction from loaded program.
         op = instr >> 12;
-
-        /*  PRINT OUT WRITTEN MEMORY (FOR DEBUGGING)
-         ******************************************************
-         *  if (op > 0)
-         *      std::cout << ++ctr << ": " << op << std::endl;
-         *  if (ctr == UINT16_MAX)
-         *      break;
-         */
-
         switch(op) {
             // TODO: Implement instructions.
             case OP_BR: {
-
+                /*
+                  Test NEG, ZRO, POS conditions.
+                  bit 11, n: Test for NEG
+                  bit 10, z: Test for ZRO
+                  bit 9,  p:  Test for POS
+                  If any conditions are true, goto R_PC + PCoffset9.
+                */
+                bool nzp = (instr >> 9) & 0x7;
+                if ((nzp & reg[R_COND]) > 0) {
+                    uint16_t PCoffset9 = signExtend(instr & 0x1FF, 9);
+                    reg[R_PC] += PCoffset9;
+                }
             } break;
             case OP_ADD: {
+                /*
+                  Register mode: Store sum of values in two registers in a third register.
+                  Immediate mode: Store sum of the value in register and a 5-bit number.
+                */
                 uint16_t dr = (instr >> 9) & 0x7; // Dest. register
                 uint16_t r1 = (instr >> 6) & 0x7;
 
-                if ((instr >> 5) & 1) {
+                if ((instr >> 5) & 0x1) {
                     // Immediate mode, 5th bit is 1
-                    reg[dr] = reg[r1] + padSignedBits(instr & 0xF, 5);
+                    reg[dr] = reg[r1] + signExtend(instr & 0xF, 5);
                 } else {
                     // Register mode
                     uint16_t r2 = instr & 0x7;
                     reg[dr] = reg[r1] + reg[r2];
                 }
-                updateFlags(dr);
-                
+                updateFlags(dr); 
             } break;
             case OP_LD: {
+                /*
+                  Load contents of the address R_PC + PCoffset9
+                  into a destination register.
+                */
+                uint16_t dr = (instr >> 9) & 0x7; // Dest. register
+                uint16_t PCoffset9 = signExtend(instr & 0x1FF, 9);
 
+                reg[dr] = lc3_memread(reg[R_PC] + PCoffset9);
+                updateFlags(dr);
             } break;
             case OP_ST: {
-
+                /*
+                  Store contents of register r1 into 
+                  memory location R_PC + PCoffset9.
+                */
+                uint16_t r1 = (instr >> 9) & 0x7;
+                uint16_t PCoffset9 = signExtend(instr & 0x1FF, 9);
+                
+                lc3_memwrite(reg[R_PC] + PCoffset9, reg[r1]);
             } break;
             case OP_JSR: {
-
+                /*
+                  Store R_PC in R_R7 and set R_PC to
+                  R_PC + PCoffset11 if bit 11 is 1
+                  Value in register r1 if bit 11 is 0
+                */
+                reg[R_R7] = reg[R_PC];
+                if ((instr >> 11) & 0x1) {
+                    uint16_t PCoffset11 = signExtend(instr & 0x7FF, 11);
+                    reg[R_PC] += PCoffset11;
+                } else {
+                    uint16_t r1 = (instr >> 6) & 0x7;
+                    reg[R_PC] = reg[r1];
+                }
             } break;
             case OP_AND: {
 
@@ -149,6 +192,18 @@ int main(int argc, char *argv[]) {
             } break;
             case OP_NOT: {
 
+            } break;
+            case OP_LDI: {
+                /*
+                  Store a value in memory into a register. The 
+                  location of this value is pointed to by a memory location
+                  which is offset from the program counter by a 9-bit number.
+                */
+                uint16_t dr = (instr >> 9) & 0x7; // Dest. register
+                uint16_t PCoffset9 = signExtend(instr & 0x1FF, 9);
+
+                reg[dr] = lc3_memread(lc3_memread(reg[R_PC] + PCoffset9));
+                updateFlags(dr);
             } break;
             case OP_STI: {
 
